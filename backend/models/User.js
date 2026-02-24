2c8ee7830ecb100f4aa48d40b8aff1f0c65f315c
sudhar3084 <rsudharshan2004@gmail.com>
2026-02-24 14:28:14 +0530

    Fix db.js import path in User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 import { DataTypes } from 'sequelize';
 import bcryptjs from 'bcryptjs';
-import { sequelize } from '../config/db.js';
+import { sequelize } from '../middleware/config/db.js';
 
 const User = sequelize.define('User', {
   id: {
